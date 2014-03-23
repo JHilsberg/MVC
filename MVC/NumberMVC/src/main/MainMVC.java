20cde23394b16c7cd1e20b5625395ce2f1cf9ddf
Julian <julian.hilsberg@gmail.com>
2014-03-23 20:02:15 +0100

    Erste Implementierungen View

---
+++ MVC/NumberMVC/src/main/MainMVC.java
@@ -0,0 +1,11 @@
+package main;
+
+import controller.NumberController;
+import view.NumberGui;
+
+public class MainMVC {
+
+	public static void main(String[] args){
+		
+	}
+}
--- MVC/NumberMVC/src/view/NumberGui.java
+++ MVC/NumberMVC/src/view/NumberGui.java
@@ -1,5 +1,43 @@
 package view;
 
-public class NumberGui {
+import java.awt.BorderLayout;
+import java.util.Observable;
+import java.util.Observer;
 
+import javax.swing.*;
+
+import controller.NumberController;
+
+public class NumberGui extends JFrame implements Observer{
+	
+	private JPanel mainPanel;
+	private JButton numberButton;
+	private JButton close;
+	private JTextField numberField;
+	
+	
+	public NumberGui(NumberController control){
+		this.setTitle("MVC-Number-Example");
+		this.setSize(300, 300);
+		
+		this.mainPanel = new JPanel(new BorderLayout());
+		this.numberButton = new JButton("Zahl ‰ndern");
+		this.close = new JButton("Schlieﬂen");
+		this.numberField = new JTextField();
+		
+		this.mainPanel.add(this.numberButton, BorderLayout.EAST);
+		this.mainPanel.add(this.close, BorderLayout.EAST);
+		this.mainPanel.add(this.numberField, BorderLayout.CENTER);
+		
+		this.add(mainPanel);
+			
+		
+	}
+
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
